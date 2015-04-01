b445c3e02f5ee0faf9cf6e128a7e4f38b2678741
Philip Sahli <philip@sahli.net>
2015-04-01 22:14:29 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='planet-example',
+	version="MASTER",
+	description='Reusable Django app for prototyping',
+	long_description='django-fastapp is a reusable Django app which lets you prototype apps in the browser with client- and server-side elements.',
+	url="https://github.com/fatrix/django-fastapp",
+	author="Philip Sahli",
+	author_email="philip@sahli.net",
+    py_modules = ['app'],
+    license ='MIT'
+)
